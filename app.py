3415d79edeb3cd39e4e61a0c9856a9101ca3696b
Francesco Carlucci <info@francescocarlucci.com>
2023-09-19 20:27:02 +0200

    Added save/load conversations, memory limit, temperature

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
+import json
 import typer
 import openai
 from typing_extensions import Annotated
 
 API_KEY_FILE = ".secrets/openai_key.txt"
+HISTORY_PATH = "history/"
 
 app = typer.Typer()
 
@@ -18,12 +20,21 @@ def store_api_key(api_key):
         with open(API_KEY_FILE, 'w') as file:
             file.write(f"{api_key}")
 
+def load_memory(file):
+    if os.path.exists(file):
+        with open(file, 'r') as file:
+            memory = file.read()
+        return json.loads(memory)
+
 load_api_key()
 
 @app.command()
 def chat(
     api_key: Annotated[str, typer.Option(envvar="OPENAI_API_KEY", prompt=True)],
-    model: Annotated[str, typer.Option("--model")] = "gpt-3.5-turbo"
+    model: Annotated[str, typer.Option("--model")] = "gpt-3.5-turbo",
+    memory_limit: Annotated[int, typer.Option("--memory-limit", "-ml")] = 0,
+    load: Annotated[str, typer.Option("--load")] = None,
+    temperature: Annotated[float, typer.Option("--temp", "-t")] = 0.7
     ):
 
     store_api_key(api_key)
@@ -31,6 +42,9 @@ def chat(
     openai.api_key = api_key
 
     memory = []
+
+    if load:
+        memory = load_memory(load)
 
     while True:
 
@@ -40,16 +54,27 @@ def chat(
             typer.echo("Goodbye!")
             break
 
+        if user_content.lower() == "save":
+            file_name = typer.prompt("File name") # TODO sanitize the name
+            with open(HISTORY_PATH + file_name + '.txt', 'w') as file:
+                file.write(json.dumps(memory))
+            continue
+
         memory.append({"role": "user", "content": user_content})
 
         completion = openai.ChatCompletion.create(
             model=model,
-            messages=memory
+            messages=memory,
+            temperature=temperature
         )
 
         response = completion.choices[0].message.content
 
         memory.append({"role": "assistant", "content": response})
+
+        # limit memory size
+        if memory_limit > 0:
+            memory = memory[-memory_limit:]
 
         typer.secho(response, fg=typer.colors.GREEN)
 
--- readme.md
+++ readme.md
@@ -2,9 +2,4 @@
 
 An extra-minimalistic CLI to interact with ChatGPT via OpenAI API.
 
-### Development Ideas
-
-- store/load conversations
-- set the memory size
-
 By: https://francescocarlucci.com/
